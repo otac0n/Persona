b98df556dd8bddd775480e58af7de47dceac5574
John Gietzen <otac0n@gietzen.us>
2014-05-09 08:18:44 -0700

    Created the Home controller.

---
+++ PersonaExample/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HomeController.cs" company="(none)">
+//   Copyright © 2014 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace PersonaExample.Controllers
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// An example controller.
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// An example action.
+        /// </summary>
+        /// <returns>A <see cref="ViewResult"/> object.</returns>
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
