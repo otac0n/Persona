6e4d111f4907c9b65afeccc39b574e71d754bfdd
John Gietzen <otac0n@gietzen.us>
2014-05-10 12:37:48 -0700

    Added Persona authentication.

---
+++ PersonaExample/Controllers/AuthController.cs
@@ -0,0 +1,94 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AuthController.cs" company="(none)">
+//   Copyright © 2014 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace PersonaExample.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Security;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// Provides authentication services.
+    /// </summary>
+    public class AuthController : AsyncController
+    {
+        internal static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        /// <summary>
+        /// Verifies an authentication assertion and logs the user in as an asynchronous operation using a task object.
+        /// </summary>
+        /// <param name="assertion">The assertion to verify.</param>
+        /// <returns>The task object representing the asynchronous operation.</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(string assertion)
+        {
+            var content = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                { "assertion", assertion },
+                { "audience", this.Request.Url.Scheme + "://" + this.Request.Url.Host + ":" + this.Request.Url.Port },
+            });
+
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.PostAsync("https://verifier.login.persona.org/verify", content))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var resultString = await response.Content.ReadAsStringAsync();
+                    var result = JObject.Parse(resultString);
+                    if ((string)result["status"] == "okay")
+                    {
+                        var token = Convert.ToBase64String(MachineKey.Protect(Encoding.UTF8.GetBytes(resultString), FormsAuthentication.FormsCookieName));
+
+                        this.Response.AppendCookie(new HttpCookie(FormsAuthentication.FormsCookieName)
+                        {
+                            HttpOnly = true,
+                            Secure = this.Request.Url.Scheme == "https",
+                            Path = FormsAuthentication.FormsCookiePath,
+                            Expires = Epoch.AddMilliseconds((double)result["expires"]),
+                            Value = token,
+                        });
+
+                        return new HttpStatusCodeResult(HttpStatusCode.OK, "OK");
+                    }
+                    else
+                    {
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Bad Request");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Logs the user out.
+        /// </summary>
+        /// <returns>A result indicating the response to the user.</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Logout()
+        {
+            if (this.Request.Cookies[FormsAuthentication.FormsCookieName] != null)
+            {
+                this.Response.AppendCookie(new HttpCookie(FormsAuthentication.FormsCookieName)
+                {
+                    Path = FormsAuthentication.FormsCookiePath,
+                    Expires = Epoch,
+                });
+            }
+
+            return new HttpStatusCodeResult(HttpStatusCode.OK, "OK");
+        }
+    }
+}
--- PersonaExample/Global.asax.cs
+++ PersonaExample/Global.asax.cs
@@ -8,15 +8,69 @@
 
 namespace PersonaExample
 {
+    using System;
+    using System.Security.Cryptography;
+    using System.Security.Principal;
+    using System.Text;
+    using System.Threading;
     using System.Web;
     using System.Web.Mvc;
     using System.Web.Routing;
+    using System.Web.Security;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
 
     /// <summary>
     /// The PersonaExample application.
     /// </summary>
     public class MvcApplication : HttpApplication
     {
+        /// <summary>
+        /// Authenticates the request.
+        /// </summary>
+        protected void Application_AuthenticateRequest()
+        {
+            var cookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (cookie == null)
+            {
+                return;
+            }
+
+            var email = (string)null;
+            try
+            {
+                var token = cookie.Value;
+                var resultString = Encoding.UTF8.GetString(MachineKey.Unprotect(Convert.FromBase64String(token), FormsAuthentication.FormsCookieName));
+                var result = JObject.Parse(resultString);
+                var expires = Controllers.AuthController.Epoch.AddMilliseconds((double)result["expires"]);
+                if (expires > DateTime.UtcNow)
+                {
+                    email = (string)result["email"];
+                }
+            }
+            catch (ArgumentException)
+            {
+            }
+            catch (CryptographicException)
+            {
+            }
+            catch (FormatException)
+            {
+            }
+            catch (JsonReaderException)
+            {
+            }
+
+            if (email == null)
+            {
+                return;
+            }
+
+            var identity = new GenericIdentity(email);
+
+            Thread.CurrentPrincipal = HttpContext.Current.User = new GenericPrincipal(identity, new string[0]);
+        }
+
         /// <summary>
         /// Starts the application.
         /// </summary>
