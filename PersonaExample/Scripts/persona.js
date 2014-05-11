90647b70961488b7bebbfd6a3497fe37be2906a8
John Gietzen <otac0n@gietzen.us>
2014-05-10 19:39:39 -0700

    Moved persona script into its own file.

---
+++ PersonaExample/Scripts/persona.js
@@ -0,0 +1,40 @@
+﻿Auth = Auth || {};
+(function ($, undefined) {
+    $(function () {
+        $('.login').click(function () { navigator.id.request(); });
+        $('.logout').click(function () { navigator.id.logout(); });
+    });
+
+    var token = { "__RequestVerificationToken": $("[name='__RequestVerificationToken']").val() };
+
+    navigator.id.watch({
+        loggedInUser: Auth.loggedInUser,
+        onlogin: function (assertion) {
+            $.ajax({
+                type: 'POST',
+                url: '/auth/login',
+                data: $.extend({}, token, { assertion: assertion }),
+                success: function (res, status, xhr) { window.location.reload(); },
+                error: function (xhr, status, err) {
+                    navigator.id.logout();
+                    if (typeof Auth.error === "function") {
+                        Auth.error(err, 'login');
+                    }
+                }
+            });
+        },
+        onlogout: function () {
+            $.ajax({
+                type: 'POST',
+                url: '/auth/logout',
+                data: $.extend({}, token),
+                success: function (res, status, xhr) { window.location.reload(); },
+                error: function (xhr, status, err) {
+                    if (typeof Auth.error === "function") {
+                        Auth.error(err, 'logout');
+                    }
+                }
+            });
+        }
+    });
+})(jQuery);
