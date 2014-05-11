b603a6636ffe5bfa8ae444e409e55637ab06000e
John Gietzen <otac0n@gietzen.us>
2014-05-11 09:55:17 -0700

    Perform authentication in a global filter.

---
+++ Persona/PersonaAuthenticationFilter.cs
@@ -0,0 +1,58 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PersonaAuthenticationFilter.cs" company="(none)">
+//   Copyright © 2014 John Gietzen.  All Rights Reserved.
+//   This source is subject to the MIT license.
+//   Please see license.md for more information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Persona
+{
+    using System;
+    using System.Security.Principal;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Security;
+
+    /// <summary>
+    /// Provides authentication via a global filter.
+    /// </summary>
+    public class PersonaAuthenticationFilter : IAuthorizationFilter
+    {
+        private readonly string audience;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PersonaAuthenticationFilter"/> class.
+        /// </summary>
+        public PersonaAuthenticationFilter(string audience)
+        {
+            this.audience = audience;
+        }
+
+        /// <summary>
+        /// Called when authorization is required.
+        /// </summary>
+        /// <param name="filterContext">The filter context.</param>
+        public virtual void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext == null)
+            {
+                throw new ArgumentNullException("filterContext");
+            }
+
+            HttpCookie newCookie;
+            var identity = new PersonaAuth().Authenticate(filterContext.HttpContext.Request.Cookies, audience, out newCookie);
+
+            if (identity != null)
+            {
+                var roles = Roles.Enabled ? Roles.GetRolesForUser(identity.Name) : new string[0];
+                filterContext.HttpContext.User = new GenericPrincipal(identity, roles);
+            }
+
+            if (newCookie != null)
+            {
+                filterContext.HttpContext.Response.AppendCookie(newCookie);
+            }
+        }
+    }
+}
--- PersonaExample/Global.asax.cs
+++ PersonaExample/Global.asax.cs
@@ -9,8 +9,6 @@
 namespace PersonaExample
 {
     using System.Configuration;
-    using System.Security.Principal;
-    using System.Threading;
     using System.Web;
     using System.Web.Helpers;
     using System.Web.Mvc;
@@ -23,30 +21,11 @@ namespace PersonaExample
     public class MvcApplication : HttpApplication
     {
         /// <summary>
-        /// Authenticates the request.
-        /// </summary>
-        protected virtual void Application_AuthenticateRequest()
-        {
-            HttpCookie newCookie;
-            var identity = new PersonaAuth().Authenticate(HttpContext.Current.Request.Cookies, ConfigurationManager.AppSettings["PersonaAudience"], out newCookie);
-
-            if (identity != null)
-            {
-                var roles = new string[0];  // TODO: Get the roles for the given identity.
-                Thread.CurrentPrincipal = HttpContext.Current.User = new GenericPrincipal(identity, roles);
-            }
-
-            if (newCookie != null)
-            {
-                HttpContext.Current.Response.AppendCookie(newCookie);
-            }
-        }
-
-        /// <summary>
         /// Starts the application.
         /// </summary>
         protected virtual void Application_Start()
         {
+            GlobalFilters.Filters.Add(new PersonaAuthenticationFilter(ConfigurationManager.AppSettings["PersonaAudience"]));
             AntiForgeryConfig.SuppressIdentityHeuristicChecks = true;
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
