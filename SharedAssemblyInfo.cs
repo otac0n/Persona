61fe40cf779c6b820f026a8d5e6c15ffc50da983
John Gietzen <otac0n@gietzen.us>
2014-05-12 22:08:43 -0700

    Incremented version number.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2014 John Gietzen")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
